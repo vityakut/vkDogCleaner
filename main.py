252f6a4a6e6e133fe375bc1adc99a6d9c715356b
Kutyavin Viktor <vityakut@gmail.com>
2019-01-18 14:25:56 +0300

    calculate remove count

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
 import vk_api
-import json
 import time
 import qtoml
-import requests
-import sys
-import random
 import os
+
 global config
 global session
-
 global tmp_path
-
 
 
 def captcha_handler(captcha):
@@ -18,20 +13,23 @@ def captcha_handler(captcha):
 	key = input("Enter captcha code {0}: ".format(url)).strip()
 	return captcha.try_again(key)
 
-def log_to_file(str, file="log"):
-	# path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "tmp")
+
+def log_to_file(arrlog, file="log"):
 	if not os.path.isdir(tmp_path):
 		os.makedirs(tmp_path)
-
 	file = open(os.path.join(tmp_path, file), 'a')
-	for line in str:
+	for line in arrlog:
 		file.write("%s\n" % line)
 	file.close()
 
+
 def vk_login():
 	global session
-	session = vk_api.VkApi(login=config['login'], password=config['password'], app_id=config['app']['id'],
-						   captcha_handler=captcha_handler)
+	session = vk_api.VkApi(
+		login=config['login'],
+		password=config['password'],
+		app_id=config['app']['id'],
+		captcha_handler=captcha_handler)
 	try:
 		session.auth()
 		print("Login complete")
@@ -39,6 +37,7 @@ def vk_login():
 	except vk_api.AuthError as error_msg:
 		print(error_msg)
 		return
+
 
 def get_config(conf_file='config.toml'):
 	global config
@@ -50,9 +49,9 @@ def get_config(conf_file='config.toml'):
 	else:
 		exit(str(os.path.join(curr_path, conf_file)) + " not found")
 
+
 def get_blocked_users_from_tmp(gid):
 	file_tmp = os.path.join(tmp_path, "%s.tmp" % str(gid))
-	arr = []
 	if os.path.isfile(file_tmp):
 		file = open(file_tmp, 'r')
 		arr = [row.strip() for row in file]
@@ -61,7 +60,6 @@ def get_blocked_users_from_tmp(gid):
 	else:
 		print(str(file_tmp) + " not found")
 		return False
-
 
 
 def get_blocked_users(gid):
@@ -99,7 +97,26 @@ def get_blocked_users(gid):
 	return banned
 
 
-
+def remove_blocked_users(gid, blocked):
+	total_blocked = len(blocked)
+	clear_mode = config['clear_mode']
+	clear_percent = config['clear_percent']
+	clear_count = config['clear_count']
+	if clear_mode == 0:
+		clear_count = total_blocked
+	elif clear_mode == 1:
+		clear_count = round((total_blocked / 100) * clear_percent)
+	elif clear_mode == 3:
+		perc_count = round((total_blocked / 100) * clear_percent)
+		if perc_count < clear_count:
+			clear_count = perc_count
+		if clear_count > total_blocked:
+			clear_count = total_blocked
+	else:
+		clear_count = config['clear_count']
+		if clear_count > total_blocked:
+			clear_count = total_blocked
+	print("Будет удалено {0} собак".format(clear_count))
 
 
 def main():
@@ -109,8 +126,12 @@ def main():
 	vk_login()
 	print("--------------")
 	for gid in config['groups']:
-		# get_blocked_users(gid)
-		get_blocked_users_from_tmp(gid)
+		# blocked = get_blocked_users(gid)
+		blocked = get_blocked_users_from_tmp(gid)
+		if blocked:
+			remove_blocked_users(gid, blocked)
+
+
 if __name__ == '__main__':
 	tmp_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "tmp")
 	main()
